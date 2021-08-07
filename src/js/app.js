420a00700808d68eb75799042c9b6019f8c93ff5
Luca <lucasantarella@hotmail.it>
2021-08-08 01:53:47 +0200

    removed lite-server + upload image

--- src/js/app.js
+++ src/js/app.js
@@ -5,11 +5,15 @@ App = {
 	web3Provider: null, //web3 provider
 	url: 'http://localhost:8545', //url for web3
 	account: '0x0',  //current Ethereum account
+	input: null,
 
+	init: function() {
 
-	init: function() { return App.initWeb3(); },
+		return App.initWeb3();
+	},
 
 	initWeb3: function(){
+		console.log("Hello");
 		/* initialize Web3 */
 		if(typeof web3 != 'undefined') { //check whether exists a provider, e.g. Metamask
 			App.web3Provider = window.ethereum; //standard since 2/11/18
@@ -20,9 +24,11 @@ App = {
 			catch(error) { console.log(error); }
 		}
 		else {	//otherwise, create a new local instance of Web3
+		console.log("local instance of Web3");
 			App.web3Provider = new Web3.providers.HttpProvider(App.url);
 			web3 = new Web3(App.web3Provider);
 		}
+		return App.initContract();
 	},
 
 	initContract: function(){
@@ -30,24 +36,61 @@ App = {
 		//Store ETH current account
 		web3.eth.getCoinbase(function(err,account) {
 			if(err == null) {
-				App.account = Aaccount;
+				App.account = account;
 				console.log(account);
 
 			}
 		});
 
-		$.getJSON('NFT.json').done(function(NFTContract) {
+		$.getJSON('NFTCollection.json').done(function(NFTContract) {
 
 			App.contracts["NFT"] = TruffleContract(NFTContract);
 			App.contracts["NFT"].setProvider(App.web3Provider);
 			console.log("All done!");
 		});
+
+		return App.listenForEvents();
+	},
+
+	listenForEvents: function(){
+		App.input = document.querySelector('input');
+		App.input.addEventListener('change', App.createNewNFT);
+	},
+
+	createNewNFT: function(){
+		const curFiles = App.input.files;
+		file = curFiles[0];
+		console.log(file);
+		var fd = new FormData();
+		fd.append('imageNFT', file, 'pingu.jpeg');
+		console.log(fd);
+		// var reader = new FileReader();
+		// reader.readAsDataURL(file);
+		// console.log(reader.result);
+		// console.log(JSON.stringify(reader.result));
+		// var formData = new FormData();
+		// formData.append('fname', file.name);
+		// formData.append('data', file);
+		//console.log(formData);
+		$.ajax({
+  		type: "POST",
+  		url: "/upload",
+  		data: fd,
+			contentType: false,
+			processData: false,
+			success: function(data, textStatus, jqXHR) {
+				alert('Success!');
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				alert('Error occurred!');
+			},
+		});
+		console.log(URL.createObjectURL(file));
 	}
 }
 
+
 //Call init whenever the window loads
 $(function() {
-	$(window).on('load', function () {
 		App.init();
-	});
 });
